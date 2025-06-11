b727f4f1d252e74d2f153fb0637b999de7717682
TatsunoriMarumo <t.maru0910@gmail.com>
2025-06-11 00:44:43 -0700

    change base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
+  base: "./",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
