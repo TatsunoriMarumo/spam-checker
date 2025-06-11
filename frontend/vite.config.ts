5f3230a2422de474d78a7bac180a3f3ac885d999
TatsunoriMarumo <t.maru0910@gmail.com>
2025-06-11 02:06:07 -0700

    change base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  base: "./",
+  base: "/spam-checker",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
