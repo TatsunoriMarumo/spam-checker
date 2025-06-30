9d20e1db10ffa04f851be4bdf0e5224853b180a8
TatsunoriMarumo <t.maru0910@gmail.com>
2025-06-29 17:56:33 -0700

    update title

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -3,8 +3,8 @@ import SpamChecker from "components/spam-checker";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Spam Checker" },
+    { name: "description", content: "Check if a message is spam or not" },
   ];
 }
 
